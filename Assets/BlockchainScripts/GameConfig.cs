2c5f9fc6b4f8f5eccba8a64cb85702bc3253e2f8
lunanga486 <lunanga486@gmail.com>
2025-08-02 13:40:46 +0700

    Initial Commit
    Initial Commit

---
+++ Assets/BlockchainScripts/GameConfig.cs
@@ -0,0 +1,12 @@
+﻿public static class GameConfig
+{
+    // Địa chỉ contract ERC721
+    public static readonly string NFTTokenGateContractAddress = "0x4E1AE8D589e98a484Be9Ee55C67B8e3CFD3Fa0e3";
+
+    // Địa chỉ contract ERC721 khác (nếu khác với TokenGate)
+    public static readonly string NFTVIPContractAddress = "0x9961B91e4A114C855F48A7c8D2eFca9F120c6a71";
+
+    // Địa chỉ contract ERC20 - GEM Token
+    public static readonly string TokenGemContractAddress = "0xCd4af99f2d03994fe0942fd5F6dF69B134F907cc";
+
+}
